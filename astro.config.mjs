975949dbfadd00724386157728765e25135c5c85
domkirby <25136584+domkirby@users.noreply.github.com>
2026-03-05 11:25:23 -0700

    Buttons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import cloudflare from '@astrojs/cloudflare';
+import remarkWpButtons from './src/plugins/remark-wp-buttons.mjs';
 
 export default defineConfig({
   site: 'https://domkirby.com',
   output: 'static',
   integrations: [tailwind()],
   adapter: cloudflare(),
+  markdown: {
+    remarkPlugins: [remarkWpButtons],
+  },
 });
